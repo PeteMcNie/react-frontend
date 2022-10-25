951554e51b3ed1cbaf2a85bd800de0ad2e95d2af
petemcnie <petemcnie0@gmail.com>
2022-10-26 08:34:40 +1300

    Remove unused redux files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-import { PostsList } from '../features/posts/PostsList'
-import { AddPostForm } from '../features/posts/AddPostForm'
-
 const App: React.FunctionComponent = () => {
     return (
         <div>
-            <h1>GOOD MORNING</h1>
-            <React.Fragment>
-                <AddPostForm />
-                <PostsList />
-            </React.Fragment>
+            <h1>GOOD MORNING React</h1>
         </div>
 
     )
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-
 import App from './components/App'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root') as HTMLElement
 )
