f7fbe30d0ddf050e51141d29e9be0e37f3ab74b3
petemcnie <petemcnie0@gmail.com>
2022-10-26 20:52:47 +1300

    Update App with react router dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+// import { Route } from 'react-router-dom'
+
+// import Title from ''
 
 const App: React.FunctionComponent = () => {
     return (
-        <div>
-            <h1>GOOD MORNING React</h1>
-        </div>
-
+        <>
+            <div>
+                <h1>GOOD MORNING React</h1>
+            </div>     
+            {/* <Route exact path='/' component={Title} /> */}
+        </>
     )
 }
 
