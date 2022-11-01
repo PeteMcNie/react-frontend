6af261ccb2a6211ad6c5a57b61707ce8be228a4e
petemcnie <petemcnie0@gmail.com>
2022-11-01 14:17:39 +1300

    Add proxy and queryclient

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Index from './home/Home'
 
+const queryClient = new QueryClient()
+
 const App: React.FunctionComponent = () => {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <Route exact path='/' component={Index} />
-        </>
+        </QueryClientProvider>
     )
 }
 
